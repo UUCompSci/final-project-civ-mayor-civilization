d3199f8fdb1b0ac0d68b1d89321f0ddaecc54264
fakemccoy1800 <fakemccoy11@gmail.com>
2025-12-03 15:14:57 -0600

    Add files via upload

--- Models/Units.cs
+++ Models/Units.cs
@@ -1,13 +1,58 @@
 // Units are the things that can move and perform actions. Units make buildings. Buildings make Units.
 namespace Final_Project_Civ_Mayor_Civilization.Models
 {
+
+    public enum CurrentTasks
+    {
+        Gather,
+
+        Scouting,
+
+        Attack,
+
+        BuildingHouse,
+
+        BuildingBarracks,
+
+        BuildingTownCenter,
+
+        nothing
+
+    }
+
     public class Unit : WorldObject
     {
         public int AttackPower { get; private set; }
         public int DefensePower { get; private set; } 
 
+        public CurrentTasks CTasks {get; private set;}
+
         // also need hitpoints for units both enemy and allys. 
     }
+
+    public class villager : Unit
+    {
+
+        public int AttackPower = 10;
+        public int DefensePower = 10;
+
+        public CurrentTasks CTasks = CurrentTasks.Nothing;
+
+        public villager(){}
+        
+    }
+
+    public class scout : Unit
+    {
+        public int AttackPower = 20;
+        public int DefensePower = 10;
+
+        public CurrentTasks CTasks = CurrentTasks.Nothing;
+
+        public scout(){}
+
+    }
+
 } 
 
 // You can get rid of what is here and make your own version of it i just put all this here as a placeholder. 
